555bee74a2d29e853126adbc56f1cd0c34cacd0c
Mario <zalac.mario1@gmail.com>
2026-04-17 16:04:34 +0200

    TODO4: JWT Filter är klar

--- src/main/java/com/example/demo/security/SecurityConfig.java
+++ src/main/java/com/example/demo/security/SecurityConfig.java
@@ -6,9 +6,15 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
+    private final JwtFilter jwtFilter;
+
+    public SecurityConfig(JwtFilter jwtFilter) {
+        this.jwtFilter = jwtFilter;
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -21,12 +27,14 @@ public class SecurityConfig {
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/", "/index.html", "/public", "/login").permitAll()
-                        .anyRequest().authenticated())
+                        .requestMatchers("/", "/index.html", "/public", "/login")
+                        .permitAll()
+                        .anyRequest().authenticated());
         // TODO:
         // Lägg till JWT filter
+        http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+
         // Lägg till oauth2Login()
-        ;
 
         return http.build();
     }
